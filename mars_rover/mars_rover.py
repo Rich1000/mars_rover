e0b4bf7eca6520ddd1b88d91220d59da551abdbe
Rich <richard@zeelo.co>
2021-03-28 14:52:21 +0100

    add basic grid wrapping functionality

--- mars_rover/mars_rover.py
+++ mars_rover/mars_rover.py
@@ -2,12 +2,18 @@ from typing import List, Tuple
 
 
 class Rover:
-    def __init__(self, start_position: Tuple[float, float, str]) -> None:
+    def __init__(
+        self,
+        start_position: Tuple[float, float, str],
+        grid_dimensions: Tuple[float, float],
+    ) -> None:
+
         self.x = start_position[0]
         self.y = start_position[1]
         self.compass_to_angle = {"N": 90, "E": 0, "S": 270, "W": 180}
-        self.angle_to_compass = {v: k for k, v in self.compass_to_angle.items()}
         self.angle = self.compass_to_angle[start_position[2]]
+        self.angle_to_compass = {v: k for k, v in self.compass_to_angle.items()}
+        self.max_x, self.max_y = grid_dimensions
 
     @property
     def position(self) -> Tuple[float, float, str]:
@@ -22,15 +28,38 @@ class Rover:
 
     def move(self, direction: str) -> None:
         distance = 1 if direction == "F" else -1
-        if self.angle == 0:
-            self.x += distance
-        elif self.angle == 90:
-            self.y += distance
-        elif self.angle == 180:
-            self.x -= distance
+
+        if self.angle in (0, 180):
+            self.move_along_x(distance)
         else:
-            self.y -= distance
+            self.move_along_y(distance)
 
     def rotate(self, direction: str) -> None:
         turn = 90 if direction == "L" else -90
         self.angle = (self.angle + turn) % 360
+
+    def move_along_x(self, distance: int) -> None:
+        x_distance = self.rover_distance_to_x_distance(distance)
+        if self.x == self.max_x and x_distance == 1:
+            self.x = 1
+        else:
+            self.x += x_distance
+
+    def move_along_y(self, distance: int) -> None:
+        y_distance = self.rover_distance_to_y_distance(distance)
+        if self.y == self.max_y and y_distance == 1:
+            self.y = 1
+        else:
+            self.y += y_distance
+
+    def rover_distance_to_x_distance(self, distance: int) -> int:
+        if self.angle == 0:
+            return distance
+        else:
+            return -distance
+
+    def rover_distance_to_y_distance(self, distance: int) -> int:
+        if self.angle == 90:
+            return distance
+        else:
+            return -distance
--- tests/test_rover.py
+++ tests/test_rover.py
@@ -2,12 +2,12 @@ from mars_rover.mars_rover import Rover
 
 
 def test_rover_reports_start_position():
-    rover = Rover(start_position=(0, 0, "N"))
+    rover = Rover(start_position=(0, 0, "N"), grid_dimensions=(10, 10))
     assert rover.position == (0, 0, "N")
 
 
 def test_rover_rotate():
-    rover = Rover(start_position=(0, 0, "N"))
+    rover = Rover(start_position=(0, 0, "N"), grid_dimensions=(10, 10))
 
     rover.instruct(["L", "L"])
     assert rover.position == (0, 0, "S")
@@ -20,7 +20,7 @@ def test_rover_rotate():
 
 
 def test_rover_move_forward_and_back():
-    rover = Rover(start_position=(0, 0, "N"))
+    rover = Rover(start_position=(0, 0, "N"), grid_dimensions=(10, 10))
 
     rover.instruct(["F"])
     assert rover.position == (0, 1, "N")
@@ -33,7 +33,27 @@ def test_rover_move_forward_and_back():
 
 
 def test_rover_simple_path():
-    rover = Rover(start_position=(1, 1, "E"))
+    rover = Rover(start_position=(1, 1, "E"), grid_dimensions=(10, 10))
 
     rover.instruct(["F", "F", "L", "B", "L", "F"])
     assert rover.position == (2, 0, "W")
+
+
+def test_grid_wrapping_x():
+    """An instruction to move beyond the x-axis grid limits results in the rover
+    position being wrapped to the grid origin"""
+
+    rover = Rover(start_position=(10, 10, "E"), grid_dimensions=(10, 10))
+
+    rover.instruct(["F"])
+    assert rover.position == (1, 10, "E")
+
+
+def test_grid_wrapping_y():
+    """An instruction to move beyond the y-axis grid limits results in the rover
+    position being wrapped to the grid origin"""
+
+    rover = Rover(start_position=(10, 10, "N"), grid_dimensions=(10, 10))
+
+    rover.instruct(["F"])
+    assert rover.position == (10, 1, "N")
